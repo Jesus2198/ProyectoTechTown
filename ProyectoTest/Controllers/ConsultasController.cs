7b43c84a276c77e2e208b7d342e995e240f3d1c8
Jesus2198 <jesus_ale_21@hotmail.com>
2023-03-28 22:23:37 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Controllers/ConsultasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoTest;
+
+namespace ProyectoTest.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private DB_CARRITOEntities1 db = new DB_CARRITOEntities1();
+
+        // GET: Consultas
+        public ActionResult Index()
+        {
+            return View(db.Consulta.ToList());
+        }
+
+        // GET: Consultas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consulta.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Consultas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,mascota,fecha_consulta")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consulta.Add(consulta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(consulta);
+        }
+
+        // GET: Consultas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consulta.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consultas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,mascota,fecha_consulta")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consulta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consulta.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consultas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Consulta consulta = db.Consulta.Find(id);
+            db.Consulta.Remove(consulta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoTest/Models/Consulta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Models.ViewModels
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string mascota { get; set; }
+        public DateTime Fecha_consulta { get; set; }
+
+    }
+}
